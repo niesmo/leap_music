5c52d507b59c3175dd83b192cdc17dc3f9214064
Nima Esmaili Mokaram <esmailimokaram.1@osu.edu>
2015-04-18 11:54:34 -0400

    Added the song and the Wad Demo and some other stuff on the main page in terms of the styling

--- js/index.js
+++ js/index.js
@@ -1,60 +1,148 @@
+window.setVariableInterval = function (callbackFunc, timing) {
+    var variableInterval = {
+        interval: timing,
+        callback: callbackFunc,
+        stopped: false,
+        runLoop: function () {
+            if (variableInterval.stopped) return;
+            var result = variableInterval.callback.call(variableInterval);
+            if (typeof result == 'number') {
+                if (result === 0) return;
+                variableInterval.interval = result;
+            }
+            variableInterval.loop();
+        },
+        stop: function () {
+            this.stopped = true;
+            window.clearTimeout(this.timeout);
+        },
+        start: function () {
+            this.stopped = false;
+            return this.loop();
+        },
+        loop: function () {
+            this.timeout = window.setTimeout(this.runLoop, this.interval);
+            return this;
+        }
+    };
+    return variableInterval.start();
+};
+
+
 $(document).ready(function () {
-    //var gdfrTrack = musicClient.registerSong("assets/songs/gdfr.mp3");
-    var this_love_guitar_track = musicClient.registerSong("assets/songs/this_love/song.ogg");
-    var current_track = this_love_guitar_track;
 
-    /*new Howl({
-                    urls: ['songs/gdfr.mp3']
-    });*/
+   
+    var kick = new Wad({ source: 'assets/songs/kick.mp3' });
+    var bass = new Wad({
+        source: 'sine',
+        volume: .9,
+        globalReverb: true,
+        env: {
+            attack: .02,
+            decay: .1,
+            sustain: .9,
+            hold: .4,
+            release: .1
+        }
+    });
 
-
+    musicClient.createKick(1000);
 
     Leap.loop(function (frame) {
-
         gestureClient.update(frame);
-        if (frame.hands.length > 0) {
 
-            $("#palm").text("Palm position : 1. " + frame.hands[0].palmPosition[0] + " 2. " + frame.hands[0].palmPosition[1] + " 3. " + frame.hands[0].palmPosition[2]);
+        if (gestureClient.isLeftIndexFingerPressed()) {
+            var lh_height = Math.abs(gestureClient.hands.left.palmPosition[1]);
 
-            $("#index").text(gestureClient.isRightIndexFingerPressed());
+            //changing the range of the number
+            //NewValue = (((OldValue - OldMin) * 
+            //  (NewMax - NewMin)) / 
+            //  (OldMax - OldMin)) + 
+            //  NewMin
 
-            if (gestureClient.isRightIndexFingerPressed()) {
-                musicClient.play(current_track);
-            }
-            else {
-                musicClient.pause(current_track);
-            }
+            var f = parseInt((((lh_height - gestureClient.config.heightMin) *
+                (musicClient.config.kickFrequencyMax - musicClient.config.kickFrequencyMin)) /
+                (gestureClient.config.heightMax - gestureClient.config.heightMin)) +
+                musicClient.config.kickFrequencyMin);
+
+
+            //updating the kick frequnecy based on the hand height
+            musicClient.updateKickFrequency(f);
+
+            //setting the height of the bar in the UI
+            UI.setBarHeight(UI.config.bars.kick, lh_height);
+
+
+            //$("#kick p").text(f);
         }
 
-        $("#frame").text(frame.id);
     });
 });
 
 var musicClient = {
-    _debugMode: false,
+    config:{
+        kickFrequencyMin:50,
+        kickFrequencyMax: 2000,
+
+
+    },
+
+    _debugMode: true,
     _registeredSongs: new Array(),
+    
+    //music related variables
+    kickFreq: 1000,
+    kickInterval: {},
 
     //this function will register a song and 
     //will return a GUID associated with that song
-    registerSong: function (songPath) {
-        var songGUID = guid();
-        this._registeredSongs[songGUID] = {
-            song: new Howl({ urls: [songPath] }),
-            playing: false,
-            mute: false,
-        }
-        return songGUID;
-    },
-    //will play the music with guid if paused
-    play: function (guid) {
-        if (!this._registeredSongs[guid].playing) {
-            this._registeredSongs[guid].playing = true;
-            var temp = this._registeredSongs[guid].song.play();
-            this.log(guid, " Playing");
+    registerSong: function (songPaths) {
+        var retGuids = {};
 
-            console.log(temp);
+        for (song in songPaths) {
+
+            var path = songPaths[song];
+            var songGUID = guid();
+
+            this._registeredSongs[songGUID] = {
+                song: new Howl({ urls: [path], buffer: true }),
+                playing: false,
+                mute: false,
+            }
+
+            //adding the guid to the return value
+            retGuids[song]= songGUID;
+        }
+        return retGuids;
+    },
+
+    //will play the music with guid if paused
+    play: function (guids) {
+        var is_to_be_played = [];
+
+        if (typeof guids === 'object') {
+
+            for (song in guids)
+            {
+                var guid = guids[song];
+
+                if(!this._registeredSongs[guid].playing) {
+                    this._registeredSongs[guid].playing = true;
+                    this._registeredSongs[guid].song.play();
+                    this.log(guid, " Playing");
+                }
+            }
+        }
+        else {
+            var guid = guids;
+            if (!this._registeredSongs[guid].playing) {
+                this._registeredSongs[guid].playing = true;
+                this._registeredSongs[guid].song.play();
+                this.log(guid, " Playing");
+            }
         }
     },
+
     //will pause the music with guid if playing
     pause: function (guid) {
         if (this._registeredSongs[guid].playing) {
@@ -73,9 +161,8 @@ var musicClient = {
         }
     },
 
-
-    //will unmate the song
-    unmute : function(){
+    //will unmute the song
+    unmute : function(guid){
         if (this._registeredSongs[guid].mute) {
             this._registeredSongs[guid].mute = false;
             this._registeredSongs[guid].song.unmute();
@@ -85,15 +172,40 @@ var musicClient = {
 
     //this is just a logger
     log: function (data) {
-        if (this.debugMode)
+        if (this._debugMode)
             console.log(data);
-    }
+    },
 
+    //will create a beat
+    createKick: function (freq) {
+        var kick = new Wad({
+            source: 'assets/songs/kick.mp3',
+            reverb: {
+                impulse: 'assets/songs/longhall.wav',
+                wet: .9
+            }
+        });
+        this.kickFreq = freq;
+
+        this.kickInterval = setVariableInterval(function () {
+            kick.play();
+        }, freq);
+    },
+
+    //update the frequency of the currently playing kick
+    updateKickFrequency: function (freq) {
+        this.kickInterval.interval = freq;
+    },
 }
 
 var gestureClient = {
     config:{
-        hands:['right','left'],
+        hands: ['right', 'left'],
+        pressedThreshold: 15,
+
+        //Leap Motions Constants
+        heightMin: 20,
+        heightMax: 600
     },
 
     data: {},
@@ -115,14 +227,48 @@ var gestureClient = {
             this.hands[lp_data.hands[1].type] = lp_data.hands[1];
         }
     },
+
+    //right hand functions
     isRightIndexFingerPressed: function () {
-        var pressedThreshold = 10;
         if(this.hands.right != undefined){
-            if (this.hands.right.palmPosition[1] - this.hands.right.fingers[1].tipPosition[1] > pressedThreshold)
+            if (this.hands.right.palmPosition[1] - this.hands.right.fingers[1].tipPosition[1] > this.config.pressedThreshold)
                 return true;
         }
         return false;
-    }
+    },
+    isRightMiddleFingerPressed: function () {
+        var pressedThreshold = 10;
+        if(this.hands.right != undefined){
+            if (this.hands.right.palmPosition[1] - this.hands.right.fingers[2].tipPosition[1] > pressedThreshold)
+                return true;
+        }
+        return false;
+    },
+
+
+    //left hand functions
+    isLeftIndexFingerPressed: function () {
+        if (this.hands.left != undefined) {
+            if (this.hands.left.palmPosition[1] - this.hands.left.fingers[1].tipPosition[1] > this.config.pressedThreshold)
+                return true;
+        }
+        return false;
+    },
+}
+
+var UI = {
+
+    config: {
+        bars : {bass: "bass", piano : "piano", kick: "kick", fluit: "fluit"},
+        barClass : "bar",
+
+    },
+
+    //this function will set the height of the given bar
+    setBarHeight: function (bar,h) {
+        $("#" + bar + " ." + UI.config.barClass).height(h + "px");
+    },
+
 }
 
 function guid() {
